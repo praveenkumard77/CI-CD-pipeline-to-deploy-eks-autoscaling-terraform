5284e8ff28224eb22311c0931d817e2b7120c29c
praveenkumard77 <58468974+praveenkumard77@users.noreply.github.com>
2020-07-28 10:43:43 +0530

    vars file

---
+++ vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+
+variable "cluster-name" {
+  default = "terraform-eks-demo"
+  type    = string
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+}
